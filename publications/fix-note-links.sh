22a10c3a78168e50f36de9c3e73e5b6f7fb973ae
Pandelis Perakakis <perakakis@gmail.com>
2026-02-28 11:36:46 +0100

    fix: action break

--- publications/fix-note-links.sh
+++ publications/fix-note-links.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Fix escaped HTML in bibliography note fields
-# Converts &lt;br/&gt;&lt;a href="..."&gt;...&lt;/a&gt; into proper clickable links
-sed -i '' \
+sed -i.bak \
   -e 's|&lt;br/&gt;&lt;a href="\([^"]*\)"&gt;\([^&]*\)&lt;/a&gt;|<div class="oa-button-wrap"><a class="oa-button" href="\1">\2</a></div>|g' \
   _site/publications/index.html
+rm -f _site/publications/index.html.bak

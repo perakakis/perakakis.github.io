3dcf68cb47365ca63906007d816a69c2057544b3
Pandelis Perakakis <perakakis@gmail.com>
2026-03-01 21:42:02 +0100

    fix: home image

--- publications/fix-note-links.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sed -i.bak \
-  -e 's|&lt;br/&gt;&lt;a href="\([^"]*\)"&gt;\([^&]*\)&lt;/a&gt;|<div class="oa-button-wrap"><a class="oa-button" href="\1">\2</a></div>|g' \
-  _site/publications/index.html
-rm -f _site/publications/index.html.bak
